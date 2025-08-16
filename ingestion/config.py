0c9e136382516946055897533747f05256570750
Gyömörey Domonkos <69725469+domonkosgyomorey@users.noreply.github.com>
2025-08-16 18:17:35 +0200

    config fix

--- ingestion/config.py
+++ ingestion/config.py
@@ -1,16 +1,9 @@
-import os
-from enum import StrEnum
-
 from ingestion.utils.gcp_secret import get_secret
 
-CWD = os.path.abspath(os.getcwd())
 
-
-class ENVIRONMENT(StrEnum):
-    LOCAL = "local"
-    DEVELOPMENT = "development"
-    PRODUCTION = "production"
-    GITHUB = "github"
+class SplitterConfig:
+    CHUNK_SIZE: int = 1000
+    CHUNK_OVERLAP: int = 200
 
 
 class MongoConfig:
@@ -18,7 +11,6 @@ class MongoConfig:
     DB_NAME: str = "assistant"
     COLLECTION_NAME: str = "knowledge"
     VECTOR_SRACH_INDEX_NAME: str = "embedding"
-    RELEVANCE_SCORE_FN: str = "cosine"
 
 
 class GCPConfig:
@@ -26,17 +18,11 @@ class GCPConfig:
     PREFIX: str = ""
 
 
-class Tavily:
-    API_KEY = get_secret("TAVILY_API_KEY")
-
-
 class Config:
+    splitter: SplitterConfig = SplitterConfig()
     mongo: MongoConfig = MongoConfig()
     gcp: GCPConfig = GCPConfig()
-    llm: str = "gemini-2.0"
     embedding: str = "gemini-embedding-001"
-    tavily: Tavily = Tavily()
-    environment: ENVIRONMENT = ENVIRONMENT.LOCAL
 
 
 config = Config()
